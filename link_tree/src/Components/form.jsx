069f6b548c7686634f4e8a7adf2ee2b4b17adc9a
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-11-05 00:38:29 +0000

    feat: build out the form on contact page

--- link_tree/src/Components/form.jsx
+++ link_tree/src/Components/form.jsx
@@ -1,2 +1,42 @@
-import React, { Component } from 'react'
+
+import React from 'react'
+
+function Form () {
+  return (
+    <React.Fragment>
+      <form action='' method='post' id='form'>
+        <label htmlFor='firstName'>First name</label>
+        <input
+          type='text'
+          placeholder='Enter your first name'
+          id='first_name'
+              />
+              
+        <label htmlFor='lastName'>Last name</label>
+              <input type='text' placeholder='Enter your last name' id='last_name' />
+              
+        <label htmlFor='lastName'>Email</label>
+              <input type='text' placeholder='yourname@email.com' id='email' />
+              
+        <label htmlFor='TextArea'>Message</label>
+        <textarea
+          name='textArea'
+          id='message'
+          placeholder='Send me a message and i will reply you as soon as possible...'
+              ></textarea>
+              
+              <input type='checkbox' name='checkbox' id='CheckBox' />
+        <label htmlFor='checkBox'>
+          you are providing your data to Austyno who may contact you.
+              </label>
+              
+        <button type='submit' id='btn_submit'>
+          Send message
+        </button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default Form 
 
--- link_tree/src/Pages/contact.js
+++ link_tree/src/Pages/contact.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Footer from "../Components/footer";
+import Form from '../Components/form';
 
 
 
 
 const Contact = () => {
   return (
-    <div className='contactContainer'>
-      <h3>Contact Me</h3>
+    <React.Fragment>
+      <div className='contactContainer'>
+        <h3>Contact Me</h3>
+        <p>
+          Hi there, Contact Me Hi there, contact me to ask me about anything you
+          have in mind.
+        </p>
+        <Form />
+      </div>
       <Footer />
-    </div>
+    </React.Fragment>
   );
 }
 
